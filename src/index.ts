78f3db3f325f677f053b18323c0e4e32d6cd7dbd
roodbaraky <kooroshr@hotmail.com>
2024-08-08 20:20:18 +0100

    Updated CORS policy

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,33 @@
 import express, { Request, Response, NextFunction } from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import cors from 'cors';
 import { getQuestions } from './controllers/questionsController';
 
-
-
-dotenv.config()
+dotenv.config();
 export const app = express();
-app.use((req, res, next) => {
-    const allowedOrigins = ['http://127.0.0.1:5173'];
-    const origin = req.headers.origin;
-    if (origin && allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", 'true');
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
-})
+
+const corsOptions = {
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+    credentials: true,
+};
+
+
+app.use(cors(corsOptions));
+
 
 app.use(express.json());
 
+
 app.route('/')
     .get((req: Request, res: Response, next: NextFunction) => {
-        res
-            .status(200)
-            .send('Hi, API is working')
-    })
+        res.status(200).send('Hi, API is working');
+    });
 
 app.route('/questions')
     .get(getQuestions)
     .post(getQuestions)
-// .post(addQuestions)
 
 const port = process.env.PORT || 3000;
 
