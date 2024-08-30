10dfae2f6936791dd5e1de7257615292408c4a57
roodbaraky <kooroshr@hotmail.com>
2024-08-30 11:11:17 +0100

    Deleted bad cors options(?)

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,9 @@ import { checkRole } from './middleware/user';
 dotenv.config();
 export const app = express();
 
-const corsOptions = {
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173', '*', 'https://examgenie-be.onrender.com/'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', '*'],
-    credentials: true,
-};
 
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(authenticateToken)
