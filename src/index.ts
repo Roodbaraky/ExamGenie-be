e0a418b0d38ca23432bd336dac28fcf4b4a41d45
roodbaraky <kooroshr@hotmail.com>
2024-08-30 11:20:27 +0100

    Tweaked CORS again

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,10 @@ export const app = express();
 
 
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'] }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(authenticateToken)
