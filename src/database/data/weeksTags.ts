038410bed8a01a489c1aff9091a9f9473dc5c9aa
roodbaraky <kooroshr@hotmail.com>
2024-09-14 15:44:00 +0100

    Updated seed data

--- src/database/data/weeksTags.ts
+++ src/database/data/weeksTags.ts
@@ -103,4 +103,493 @@ export const weeksTags: WeeksTags[] = [
 
     { week_id: 117, tag_id: 7 },
     { week_id: 117, tag_id: 8 },
+    { week_id: 118, tag_id: 54 },  // Recap
+    { week_id: 118, tag_id: 55 },
+
+    { week_id: 119, tag_id: 56 },
+    { week_id: 119, tag_id: 57 },
+
+    { week_id: 120, tag_id: 58 },
+
+    { week_id: 121, tag_id: 59 },
+    { week_id: 121, tag_id: 60 },
+
+    { week_id: 122, tag_id: 61 },
+
+    { week_id: 123, tag_id: 62 },
+    { week_id: 123, tag_id: 63 },
+
+    { week_id: 124, tag_id: 64 },
+
+    { week_id: 125, tag_id: 65 },
+    { week_id: 125, tag_id: 66 },
+
+    { week_id: 126, tag_id: 67 },
+
+    { week_id: 127, tag_id: 68 },
+    { week_id: 127, tag_id: 69 },
+
+    { week_id: 128, tag_id: 70 },
+
+    { week_id: 129, tag_id: 71 },
+    { week_id: 129, tag_id: 72 },
+
+    { week_id: 130, tag_id: 73 },
+
+    { week_id: 131, tag_id: 74 },
+    { week_id: 131, tag_id: 75 },
+
+    { week_id: 132, tag_id: 76 },
+
+    { week_id: 133, tag_id: 77 },
+    { week_id: 133, tag_id: 78 },
+
+    { week_id: 134, tag_id: 79 },
+
+    { week_id: 135, tag_id: 80 },
+    { week_id: 135, tag_id: 81 },
+
+    { week_id: 136, tag_id: 82 },
+
+    { week_id: 137, tag_id: 83 },
+    { week_id: 137, tag_id: 84 },
+
+    { week_id: 138, tag_id: 85 },
+
+    { week_id: 139, tag_id: 86 },
+    { week_id: 139, tag_id: 87 },
+
+    { week_id: 140, tag_id: 88 },
+
+    { week_id: 141, tag_id: 89 },
+    { week_id: 141, tag_id: 90 },
+
+    { week_id: 142, tag_id: 91 },
+
+    { week_id: 143, tag_id: 92 },
+    { week_id: 143, tag_id: 93 },
+
+    { week_id: 144, tag_id: 94 },
+
+    { week_id: 145, tag_id: 95 },
+    { week_id: 145, tag_id: 96 },
+
+    { week_id: 146, tag_id: 97 },
+
+    { week_id: 147, tag_id: 98 },
+    { week_id: 147, tag_id: 99 },
+
+    { week_id: 148, tag_id: 100 },
+
+    { week_id: 149, tag_id: 101 },
+    { week_id: 149, tag_id: 102 },
+
+    { week_id: 150, tag_id: 103 },
+
+    { week_id: 151, tag_id: 104 },
+    { week_id: 151, tag_id: 105 },
+
+    { week_id: 152, tag_id: 106 },
+
+    { week_id: 153, tag_id: 107 },
+    { week_id: 153, tag_id: 108 },
+
+    { week_id: 154, tag_id: 109 },
+
+    { week_id: 155, tag_id: 110 },
+    { week_id: 155, tag_id: 111 },
+
+    { week_id: 156, tag_id: 112 },
+// generated more dummy data from hereon
+    { week_id: 157, tag_id: 113 },
+    { week_id: 157, tag_id: 114 },
+
+    { week_id: 158, tag_id: 115 },
+    { week_id: 158, tag_id: 116 },
+
+    { week_id: 159, tag_id: 117 },
+
+    { week_id: 160, tag_id: 118 },
+    { week_id: 160, tag_id: 119 },
+
+    { week_id: 161, tag_id: 120 },
+
+    { week_id: 162, tag_id: 121 },
+    { week_id: 162, tag_id: 122 },
+
+    { week_id: 163, tag_id: 123 },
+
+    { week_id: 164, tag_id: 124 },
+    { week_id: 164, tag_id: 125 },
+
+    { week_id: 165, tag_id: 126 },
+
+    { week_id: 166, tag_id: 127 },
+    { week_id: 166, tag_id: 128 },
+
+    { week_id: 167, tag_id: 129 },
+
+    { week_id: 168, tag_id: 130 },
+    { week_id: 168, tag_id: 131 },
+
+    { week_id: 169, tag_id: 132 },
+
+    { week_id: 170, tag_id: 133 },
+    { week_id: 170, tag_id: 134 },
+
+    { week_id: 171, tag_id: 135 },
+
+    { week_id: 172, tag_id: 136 },
+    { week_id: 172, tag_id: 137 },
+
+    { week_id: 173, tag_id: 138 },
+
+    { week_id: 174, tag_id: 139 },
+    { week_id: 174, tag_id: 140 },
+
+    { week_id: 175, tag_id: 141 },
+
+    { week_id: 176, tag_id: 142 },
+    { week_id: 176, tag_id: 143 },
+
+    { week_id: 177, tag_id: 144 },
+
+    { week_id: 178, tag_id: 145 },
+    { week_id: 178, tag_id: 146 },
+
+    { week_id: 179, tag_id: 147 },
+
+    { week_id: 180, tag_id: 148 },
+    { week_id: 180, tag_id: 149 },
+
+    { week_id: 181, tag_id: 150 },
+
+    { week_id: 182, tag_id: 151 },
+    { week_id: 182, tag_id: 152 },
+
+    { week_id: 183, tag_id: 153 },
+
+    { week_id: 184, tag_id: 154 },
+    { week_id: 184, tag_id: 155 },
+
+    { week_id: 185, tag_id: 156 },
+
+    { week_id: 186, tag_id: 157 },
+    { week_id: 186, tag_id: 158 },
+
+    { week_id: 187, tag_id: 159 },
+
+    { week_id: 188, tag_id: 160 },
+    { week_id: 188, tag_id: 161 },
+
+    { week_id: 189, tag_id: 162 },
+
+    { week_id: 190, tag_id: 163 },
+    { week_id: 190, tag_id: 164 },
+
+    { week_id: 191, tag_id: 165 },
+
+    { week_id: 192, tag_id: 166 },
+    { week_id: 192, tag_id: 167 },
+
+    { week_id: 193, tag_id: 168 },
+
+    { week_id: 194, tag_id: 169 },
+    { week_id: 194, tag_id: 170 },
+
+    { week_id: 195, tag_id: 171 },
+    { week_id: 196, tag_id: 172 },
+    { week_id: 196, tag_id: 173 },
+
+    { week_id: 197, tag_id: 174 },
+    { week_id: 197, tag_id: 175 },
+
+    { week_id: 198, tag_id: 176 },
+
+    { week_id: 199, tag_id: 177 },
+    { week_id: 199, tag_id: 178 },
+
+    { week_id: 200, tag_id: 179 },
+
+    { week_id: 201, tag_id: 180 },
+    { week_id: 201, tag_id: 181 },
+
+    { week_id: 202, tag_id: 182 },
+
+    { week_id: 203, tag_id: 183 },
+    { week_id: 203, tag_id: 184 },
+
+    { week_id: 204, tag_id: 185 },
+
+    { week_id: 205, tag_id: 186 },
+    { week_id: 205, tag_id: 187 },
+
+    { week_id: 206, tag_id: 188 },
+
+    { week_id: 207, tag_id: 189 },
+    { week_id: 207, tag_id: 190 },
+
+    { week_id: 208, tag_id: 191 },
+
+    { week_id: 209, tag_id: 192 },
+    { week_id: 209, tag_id: 193 },
+
+    { week_id: 210, tag_id: 194 },
+
+    { week_id: 211, tag_id: 197 },
+  
+
+    { week_id: 212, tag_id: 198 },
+
+    { week_id: 213, tag_id: 199 },
+
+    { week_id: 214, tag_id: 200 },
+
+    { week_id: 215, tag_id: 201 },
+    { week_id: 215, tag_id: 202 },
+
+    { week_id: 216, tag_id: 203 },
+
+    { week_id: 217, tag_id: 204 },
+    { week_id: 217, tag_id: 205 },
+
+    { week_id: 218, tag_id: 206 },
+
+    { week_id: 219, tag_id: 207 },
+    { week_id: 219, tag_id: 208 },
+
+    { week_id: 220, tag_id: 209 },
+
+    { week_id: 221, tag_id: 210 },
+    { week_id: 221, tag_id: 211 },
+
+    { week_id: 222, tag_id: 212 },
+
+    { week_id: 223, tag_id: 213 },
+    { week_id: 223, tag_id: 214 },
+
+    { week_id: 224, tag_id: 215 },
+
+    { week_id: 225, tag_id: 216 },
+    { week_id: 225, tag_id: 217 },
+
+    { week_id: 226, tag_id: 218 },
+
+    { week_id: 227, tag_id: 219 },
+    { week_id: 227, tag_id: 220 },
+
+    { week_id: 228, tag_id: 221 },
+
+    { week_id: 229, tag_id: 222 },
+    { week_id: 229, tag_id: 223 },
+
+    { week_id: 230, tag_id: 224 },
+
+    { week_id: 231, tag_id: 225 },
+    { week_id: 231, tag_id: 226 },
+
+    { week_id: 232, tag_id: 227 },
+
+    { week_id: 233, tag_id: 228 },
+    { week_id: 233, tag_id: 229 },
+
+    { week_id: 234, tag_id: 230 },
+
+    // Week 235 to 273
+    { week_id: 235, tag_id: 231 },
+    { week_id: 235, tag_id: 232 },
+
+    { week_id: 236, tag_id: 233 },
+    { week_id: 236, tag_id: 234 },
+
+    { week_id: 237, tag_id: 235 },
+
+    { week_id: 238, tag_id: 236 },
+    { week_id: 238, tag_id: 237 },
+
+    { week_id: 239, tag_id: 238 },
+
+    { week_id: 240, tag_id: 239 },
+    { week_id: 240, tag_id: 240 },
+
+    { week_id: 241, tag_id: 241 },
+
+    { week_id: 242, tag_id: 242 },
+    { week_id: 242, tag_id: 243 },
+
+    { week_id: 243, tag_id: 244 },
+
+    { week_id: 244, tag_id: 245 },
+    { week_id: 244, tag_id: 246 },
+
+    { week_id: 245, tag_id: 247 },
+
+    { week_id: 246, tag_id: 248 },
+    { week_id: 246, tag_id: 249 },
+
+    { week_id: 247, tag_id: 250 },
+
+    { week_id: 248, tag_id: 251 },
+    { week_id: 248, tag_id: 252 },
+
+    { week_id: 249, tag_id: 253 },
+
+    { week_id: 250, tag_id: 254 },
+    { week_id: 250, tag_id: 255 },
+
+    { week_id: 251, tag_id: 256 },
+
+    { week_id: 252, tag_id: 257 },
+    { week_id: 252, tag_id: 258 },
+
+    { week_id: 253, tag_id: 259 },
+
+    { week_id: 254, tag_id: 260 },
+    { week_id: 254, tag_id: 261 },
+
+    { week_id: 255, tag_id: 262 },
+
+    { week_id: 256, tag_id: 263 },
+    { week_id: 256, tag_id: 264 },
+
+    { week_id: 257, tag_id: 265 },
+
+    { week_id: 258, tag_id: 266 },
+    { week_id: 258, tag_id: 267 },
+
+    { week_id: 259, tag_id: 268 },
+
+    { week_id: 260, tag_id: 269 },
+    { week_id: 260, tag_id: 270 },
+
+    { week_id: 261, tag_id: 271 },
+
+    { week_id: 262, tag_id: 272 },
+    { week_id: 262, tag_id: 273 },
+
+    { week_id: 263, tag_id: 274 },
+
+    { week_id: 264, tag_id: 275 },
+    { week_id: 264, tag_id: 276 },
+
+    { week_id: 265, tag_id: 277 },
+
+    { week_id: 266, tag_id: 278 },
+    { week_id: 266, tag_id: 279 },
+
+    { week_id: 267, tag_id: 280 },
+
+    { week_id: 268, tag_id: 281 },
+    { week_id: 268, tag_id: 282 },
+
+    { week_id: 269, tag_id: 283 },
+
+    { week_id: 270, tag_id: 284 },
+    { week_id: 270, tag_id: 285 },
+
+    { week_id: 271, tag_id: 286 },
+
+    { week_id: 272, tag_id: 287 },
+    { week_id: 272, tag_id: 288 },
+
+    { week_id: 273, tag_id: 289 },
+
+    { week_id: 274, tag_id: 290 },
+    { week_id: 274, tag_id: 291 },
+
+    { week_id: 275, tag_id: 292 },
+
+    { week_id: 276, tag_id: 293 },
+    { week_id: 276, tag_id: 294 },
+
+    { week_id: 277, tag_id: 295 },
+
+    { week_id: 278, tag_id: 296 },
+    { week_id: 278, tag_id: 297 },
+
+    { week_id: 279, tag_id: 298 },
+
+    { week_id: 280, tag_id: 299 },
+    { week_id: 280, tag_id: 300 },
+
+    { week_id: 281, tag_id: 301 },
+
+    { week_id: 282, tag_id: 302 },
+    { week_id: 282, tag_id: 303 },
+
+    { week_id: 283, tag_id: 305 },
+
+    { week_id: 284, tag_id: 306 },
+
+    { week_id: 285, tag_id: 307 },
+
+    // Week 286 to 312
+    { week_id: 286, tag_id: 172 },
+    { week_id: 286, tag_id: 173 },
+
+    { week_id: 287, tag_id: 174 },
+    { week_id: 287, tag_id: 175 },
+
+    { week_id: 288, tag_id: 176 },
+
+    { week_id: 289, tag_id: 177 },
+    { week_id: 289, tag_id: 178 },
+
+    { week_id: 290, tag_id: 179 },
+
+    { week_id: 291, tag_id: 180 },
+    { week_id: 291, tag_id: 181 },
+
+    { week_id: 292, tag_id: 182 },
+
+    { week_id: 293, tag_id: 183 },
+    { week_id: 293, tag_id: 184 },
+
+    { week_id: 294, tag_id: 185 },
+
+    { week_id: 295, tag_id: 186 },
+    { week_id: 295, tag_id: 187 },
+
+    { week_id: 296, tag_id: 188 },
+
+    { week_id: 297, tag_id: 189 },
+    { week_id: 297, tag_id: 190 },
+
+    { week_id: 298, tag_id: 191 },
+
+    { week_id: 299, tag_id: 192 },
+    { week_id: 299, tag_id: 193 },
+
+    { week_id: 300, tag_id: 194 },
+
+    { week_id: 301, tag_id: 197 },
+  
+
+    { week_id: 302, tag_id: 198 },
+
+  
+    { week_id: 303, tag_id: 199 },
+
+    { week_id: 304, tag_id: 200 },
+
+    { week_id: 305, tag_id: 201 },
+    { week_id: 305, tag_id: 202 },
+
+    { week_id: 306, tag_id: 203 },
+
+    { week_id: 307, tag_id: 204 },
+    { week_id: 307, tag_id: 205 },
+
+    { week_id: 308, tag_id: 206 },
+
+    { week_id: 309, tag_id: 207 },
+    { week_id: 309, tag_id: 208 },
+
+    { week_id: 310, tag_id: 209 },
+
+    { week_id: 311, tag_id: 210 },
+    { week_id: 311, tag_id: 211 },
+
+    { week_id: 312, tag_id: 212 }
+
 ];
